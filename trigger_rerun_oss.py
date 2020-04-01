adf758d179e5f76314a3f2ad4de7c6c525b3da7e
shenya1x <yangx.shen@intel.com>
2020-04-01 14:44:41 +0800

    add trigger_rerun_oss.py
    Signed-off-by: shenya1x <yangx.shen@intel.com>

---
+++ trigger_rerun_oss.py
@@ -0,0 +1,33 @@
+import argparse
+
+
+OSS_SERVER = r'https://bios-ci.intel.com'
+
+
+def trigger_rerun_oss_task(task_id, rerun_all):
+    print('task_id:{}'.format(task_id))
+    print('rerun_all:{}'.format(rerun_all))
+    # url = r'{}/app/rest/rerun_auto_task/?auto_task_id={}&rerun_all={}'.format(OSS_SERVER, task_id, rerun_all)
+    # request = urllib2.Request(url)
+    # response = urllib2.urlopen(request)
+    # response_data = response.read()
+    # response.close()
+    # print response_data
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='trigger_rerun_oss.py',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='',
+        epilog='')
+
+    parser.add_argument('-t', '--task_id')
+    parser.add_argument('-r', '--rerun_all')
+    args = parser.parse_args()
+    trigger_rerun_oss_task(args.task_id, args.rerun_all)
+
+
+if __name__ == '__main__':
+    main()
+
