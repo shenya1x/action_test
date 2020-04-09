b6f9f9a89c37ace6dc23d536243f90858cc8d91f
shenya1x <yangx.shen@intel.com>
2020-04-09 08:59:04 +0800

    modify trigger_rerun_oss_task script
    Signed-off-by: shenya1x <yangx.shen@intel.com>

--- trigger_rerun_oss.py
+++ trigger_rerun_oss.py
@@ -6,16 +6,12 @@ OSS_SERVER = r'https://bios-ci.intel.com'
 
 
 def trigger_rerun_oss_task(task_id, rerun_all):
-    print('task_id:{}'.format(task_id))
-    print('rerun_all:{}'.format(rerun_all))
     url = r'{}/app/rest/rerun_auto_task/?auto_task_id={}&rerun_all={}'.format(OSS_SERVER, task_id, rerun_all)
-    res = requests.get(url)
-    print(res.content)
-    # request = urllib2.Request(url)
-    # response = urllib2.urlopen(request)
-    # response_data = response.read()
-    # response.close()
-    # print response_data
+    try:
+        res = requests.get(url)
+        print(res.content)
+    except Exception as e:
+        print('exception {} happens while request url {}'.format(e, url))
 
 
 def main():
@@ -24,9 +20,8 @@ def main():
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description='',
         epilog='')
-
-    parser.add_argument('-t', '--task_id')
-    parser.add_argument('-r', '--rerun_all')
+    parser.add_argument('--task_id', required=True)
+    parser.add_argument('--rerun_all', required=True)
     args = parser.parse_args()
     trigger_rerun_oss_task(args.task_id, args.rerun_all)
 
